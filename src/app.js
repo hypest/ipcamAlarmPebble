81c9bde094e940726972574d883792271702b057
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2015-08-15 02:26:46 +0300

    Able to toggle the alarm email flag

--- src/app.js
+++ src/app.js
@@ -49,7 +49,56 @@ main.on('click', 'select', function(e) {
 
   var auth = 'Basic ' + btoa(config.credentials.user + ':' + config.credentials.pass);
   var statusUrl = config.ipcam.url + '/get_params.cgi';
+  var setAlarmUrl = config.ipcam.url + '/set_alarm.cgi?next_url=alarm.htm&motion_armed=1&input_armed=0&motion_sensitivity=3&iolinkage=0&upload_interval=0&schedule_enable=0&schedule_sun_0=0&schedule_sun_1=0&schedule_sun_2=0&schedule_mon_0=0&schedule_mon_1=0&schedule_mon_2=0&schedule_tue_0=0&schedule_tue_1=0&schedule_tue_2=0&schedule_wed_0=0&schedule_wed_1=0&schedule_wed_2=0&schedule_thu_0=0&schedule_thu_1=0&schedule_thu_2=0&schedule_fri_0=0&schedule_fri_1=0&schedule_fri_2=0&schedule_sat_0=0&schedule_sat_1=0&schedule_sat_2=0';
 
+  ajax({url: statusUrl, type: 'text', headers : { Authorization: auth }},
+    function(js) {
+      console.log('status call 1 returned.');
+      console.log(js);
+
+      eval(js);
+
+      var newValue = (alarm_mail === 0 ? "1" : "0");
+
+      ajax({url: setAlarmUrl + "&mail=" + newValue, type: 'text', headers : { Authorization: auth }},
+        function(js) {
+          console.log('setAlarm call returned.');
+          console.log(js);
+    
+          ajax({url: statusUrl, type: 'text', headers : { Authorization: auth }},
+            function(js) {
+              console.log('status call 2 returned.');
+              console.log(js);
+        
+              eval(js);
+        
+              var resultsCard = new UI.Card({
+                title: 'Alarm is now',
+                body: (alarm_mail > 0 ? "* ON *" : "-- OFF --")
+              });
+
+              // Show results, remove splash card
+              resultsCard.show();
+              splashCard.hide();
+            },
+            function(error) {
+              splashCard.hide();
+              console.log('status 2 failed: ' + error);
+            }
+          );
+        },
+        function(error) {
+          splashCard.hide();
+          console.log('setAlarm failed: ' + error);
+        }
+      );
+    },
+    function(error) {
+      splashCard.hide();
+      console.log('status 1 failed: ' + error);
+    }
+  );
+/*
   ajax({url: statusUrl, type: 'text', headers : { Authorization: auth }},
     function(js) {
       console.log('Ajax call returned.');
@@ -70,7 +119,7 @@ main.on('click', 'select', function(e) {
       console.log('Ajax failed: ' + error);
     }
   );
-
+*/
 });
 
 main.on('click', 'down', function(e) {
