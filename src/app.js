5084766e0b8c09c774dff60ea763d3b333ad4dfe
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2015-08-15 01:13:37 +0300

    Based on tutorial and authing with the ipcam

---
+++ src/app.js
@@ -0,0 +1,84 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+// var Base64 = require('base64');
+var creds = require('credentials');
+
+var main = new UI.Card({
+  title: 'Pebble.js',
+  icon: 'images/menu_icon.png',
+  subtitle: 'Hello World!',
+  body: 'Press any button.'
+});
+
+main.show();
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'Pebble.js',
+        icon: 'images/menu_icon.png',
+        subtitle: 'Can do Menus'
+      }, {
+        title: 'Second Item',
+        subtitle: 'Subtitle Text'
+      }]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
+
+main.on('click', 'select', function(e) {
+  var ajax = require('ajax');
+  
+  // Show splash
+  var splashCard = new UI.Card({
+    title: "Please Wait",
+    body: "Downloading..."
+  });
+  splashCard.show();
+
+  var auth = 'Basic ' + btoa(creds.user + ':' + creds.pass);
+  var statusUrl = 'http://192.168.1.69/get_params.cgi';
+//   console.log('auth ' + auth);
+
+  ajax({url: statusUrl, type: 'text', headers : { Authorization: auth }},
+    function(js) {
+      console.log('Ajax call returned.');
+      // Data is supplied here
+      eval(js);
+      
+      // Use data to show a weather forecast Card
+      var resultsCard = new UI.Card({
+        title: 'datum',
+        body: mail_svr
+      });
+    
+      // Show results, remove splash card
+      resultsCard.show();
+      splashCard.hide();
+    },
+    function(error) {
+      console.log('Ajax failed: ' + error);
+    }
+  );
+
+});
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('A Card');
+  card.subtitle('Is a Window');
+  card.body('The simplest window type in Pebble.js.');
+  card.show();
+});
