3558ca3588e6aa49edcc7d97950ec0b1013548f9
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2015-08-15 03:28:58 +0300

    Result on main screen, autohide

--- src/app.js
+++ src/app.js
@@ -14,12 +14,12 @@ var ajax = require('ajax');
 var statusUrl = config.ipcam.url + '/get_params.cgi';
 var setAlarmUrl = config.ipcam.url + '/set_alarm.cgi?next_url=alarm.htm&motion_armed=1&input_armed=0&motion_sensitivity=3&iolinkage=0&upload_interval=0&schedule_enable=0&schedule_sun_0=0&schedule_sun_1=0&schedule_sun_2=0&schedule_mon_0=0&schedule_mon_1=0&schedule_mon_2=0&schedule_tue_0=0&schedule_tue_1=0&schedule_tue_2=0&schedule_wed_0=0&schedule_wed_1=0&schedule_wed_2=0&schedule_thu_0=0&schedule_thu_1=0&schedule_thu_2=0&schedule_fri_0=0&schedule_fri_1=0&schedule_fri_2=0&schedule_sat_0=0&schedule_sat_1=0&schedule_sat_2=0';
 
-var main = new UI.Card({
+var mainCard = new UI.Card({
   title: 'lockAlarm',
   body: 'Press up to lock or down to unlock.'
 });
 
-main.show();
+mainCard.show();
 
 var waitingCard = new UI.Card({
   title: "Please Wait",
@@ -37,32 +37,34 @@ function alarmMailTo(newValue) {
               function(js) {
                 eval(js);
 
-                var resultsCard = new UI.Card({
-                  title: 'Alarm is now',
-                  body: (alarm_mail > 0 ? "* ON *" : "-- OFF --")
-                });
+                mainCard.title("Alarm is now");
+                mainCard.body((alarm_mail > 0 ? "* ON *" : "-- OFF --"));
 
-                // Show results, remove the waiting card
-                resultsCard.show();
                 waitingCard.hide();
+
+                setTimeout(function() {
+                  mainCard.hide();
+                }, 1000);
               },
               function(error) {
+                mainCard.body("An error has occured while fetching status.");
                 waitingCard.hide();
                 console.log('status 2 failed: ' + error);
               }
              );
        },
        function(error) {
+         mainCard.body("An error has occured while toggling alarm.");
          waitingCard.hide();
          console.log('setAlarm failed: ' + error);
        }
   );
 }
 
-main.on('click', 'up', function(e) {
+mainCard.on('click', 'up', function(e) {
   alarmMailTo(1);
 });
 
-main.on('click', 'down', function(e) {
+mainCard.on('click', 'down', function(e) {
   alarmMailTo(0);
 });
